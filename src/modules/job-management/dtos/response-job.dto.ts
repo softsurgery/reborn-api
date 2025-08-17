9e8dc48716a4c62056899f76103a93707968f360
Houssem Eddine Chibouni <houssemeddine.chibouni@fsb.ucar.tn>
2025-08-17 16:49:39 +0100

    fix(job-management): expose repsonse attributes & add user response dto

--- src/modules/job-management/dtos/response-job.dto.ts
+++ src/modules/job-management/dtos/response-job.dto.ts
@@ -1,13 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import { ResponseUserDto } from 'src/modules/user-management/dtos/user/response-user.dto';
 import { ResponseDtoHelper } from 'src/shared/database/dtos/database.response.dto';
 
 export class ResponseJobDto extends ResponseDtoHelper {
   @ApiProperty({ type: String })
+  @Expose()
   title: string;
 
   @ApiProperty({ type: String })
+  @Expose()
   description: string;
 
   @ApiProperty({ type: Number })
+  @Expose()
   price: number;
+
+  @ApiProperty({ type: ResponseUserDto })
+  @Expose()
+  @Type(() => ResponseUserDto)
+  postedBy: ResponseUserDto;
 }
