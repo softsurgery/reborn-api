871bcf76e758aa72a35823736d1ba01f54a52906
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-04-04 16:20:48 +0000

    feat(reference-type): implement save method to generate ID from label

--- src/shared/reference-types/services/ref-type.service.ts
+++ src/shared/reference-types/services/ref-type.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { RefTypeRepository } from '../repositories/ref-type.repository';
 import { RefTypeEntity } from '../entities/ref-type.entity';
 import { AbstractCrudService } from 'src/shared/database/services/abstract-crud.service';
+import { DeepPartial } from 'typeorm';
 
 @Injectable()
 export class RefTypeService extends AbstractCrudService<RefTypeEntity> {
   constructor(private readonly refTypeRepository: RefTypeRepository) {
     super(refTypeRepository);
+  }
+  async save(dto: DeepPartial<RefTypeEntity>): Promise<RefTypeEntity> {
+    if (!dto.id) {
+      dto.id = dto.label?.toLowerCase().replace(/\s+/g, '-') || '';
+    }
+    return this.refTypeRepository.save(dto);
   }
 
   async findByLabel(label: string): Promise<RefTypeEntity | null> {
